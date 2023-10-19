dcf629426dce15affa45196897e10763a51749d0
axla <axla@gft.com>
2023-10-19 15:13:45 +0200

    Logic done

--- src/main/java/com/gfttraining/productAPI/services/ProductService.java
+++ src/main/java/com/gfttraining/productAPI/services/ProductService.java
@@ -39,7 +39,7 @@ public class ProductService {
 
     }
 
-    public Product updateProduct (long id, ProductRequest productRequest) throws NonExistingProductException{
+    public Product updateProduct (long id, ProductRequest productRequest) throws NonExistingProductException {
 
     	Category category = categoryRepository.findById(productRequest.getCategory()).orElse(categoryRepository.findById("other").get());
 
@@ -67,14 +67,15 @@ public class ProductService {
             productRepository.deleteById(id);
          }
 
-
     }
 
     public List<Product> listProducts() {
         return productRepository.findAll();
     }
 
-
+    public List<Product> listProductsByNameContainsIgnoreCase(String name) {
+        return productRepository.findByNameIgnoreCaseContaining(name);
+    }
 
     public Product listProductById(long id) throws NonExistingProductException {
         return productRepository.findById(id)
