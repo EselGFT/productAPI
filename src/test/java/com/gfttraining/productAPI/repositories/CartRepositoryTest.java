f2c438082b9756de6f2bffc43ec56863c792b477
axla <axla@gft.com>
2023-10-24 14:09:52 +0200

    Cleaned and port made dynamic

--- src/test/java/com/gfttraining/productAPI/repositories/CartRepositoryTest.java
+++ src/test/java/com/gfttraining/productAPI/repositories/CartRepositoryTest.java
@@ -2,8 +2,6 @@ package com.gfttraining.productAPI.repositories;
 
 import com.gfttraining.productAPI.exceptions.InvalidCartConnectionException;
 import com.gfttraining.productAPI.model.ProductDTO;
-import com.gfttraining.productAPI.services.ProductService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
